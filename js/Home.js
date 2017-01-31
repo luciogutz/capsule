8f84f0f67f932dd4c3677bc79e5fd37a3eb50e2f
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-01-31 15:30:06 -0600

    workingout the functionality of google authentication and routing to another page

--- js/Home.js
+++ js/Home.js
@@ -4,6 +4,9 @@ export default React.createClass({
   render() {
     return(
       <section>
+        <header>
+          <button> Sign Out </button>
+        </header>
         <h1>hello</h1>
       </section>
     )
--- js/Landing.js
+++ js/Landing.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import ajax from 'jquery'
 import Reactfire from 'reactfire'
-
+import { Link } from 'react-router'
 
 
 export default React.createClass({
   getInitialState() {
     return {
-      user: "",
-      password: ""
+      provider: () => {},
+      currentName: "Not logged in",
+      name: "",
+      picture: ""
     }
+  },
+  componentDidMount() {
+    this.setState({provider: new firebase.auth.GoogleAuthProvider()})
+  },
+  signUserIn() {
+    firebase.auth().signInWithRedirect(this.state.provider)
+    firebase.auth().getRedirectResult().then((result) => {
+      if(result.credential) {
+        var token = result.credential.accessToken
+      }
+      var user = result.user
+    }).catch((error) => {
+      var errorCode = error.code
+      var errorMessage = error.message
+      var email = error.email
+      var credential = error.credential
+      console.log("ERROR authenticating with firebase: " + errorMessage)
+      //FIXME: Better logging/error handling
+    })
   },
   render() {
     return(
@@ -17,11 +38,8 @@ export default React.createClass({
         <h1 className="capsule"> CAPSULE </h1>
         <h4 className="subTitle"> Dont let time get away from you. </h4>
         <p className="started"> Let's get started </p>
-        <button className="googleSignIn"> Sign in with your Google Account </button>
-        <form className="signInForm">
-          <input className="gmail" placeholder="gmail" type="email"/>
-          <input className="gmail" placeholder="password" type="password"/>
-        </form>
+        <button onClick={this.signUserIn} className="googleSignIn"> Sign in with your Google Account </button>
+        <p>{this.state.currentName}</p>
       </section>
     )
   }
