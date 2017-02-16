828abc49c56f0bf7608c3cae73867a80c814cba9
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-02-16 14:05:49 -0600

    added Modal.js

---
+++ js/Modal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Reactfire from 'reactfire'
+import Firebase from 'firebase'
+import {browserHistory, Link} from 'react-router'
+
+
+export default React.createClass({
+  render() {
+    return (
+      <section>
+        
+      </section>
+    )
+  }
+})
